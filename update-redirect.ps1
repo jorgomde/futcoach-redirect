8f8ee142afe8658be56af870dca2138f422866aa
jorgomde <91954553+jorgomde@users.noreply.github.com>
2025-08-04 03:50:36 +0200

    Update update-redirect.ps1

--- update-redirect.ps1
+++ update-redirect.ps1
@@ -5,21 +5,20 @@ Start-Sleep -Seconds 5
 $response = Invoke-RestMethod -Uri http://127.0.0.1:4040/api/tunnels
 $url = $response.tunnels[0].public_url + "/api/"
 
-$redirect = @"
+$html = @"
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv='refresh' content='0; url=$url' />
-    <title>Redireccionando a backend...</title>
+    <title>Backend URL</title>
   </head>
   <body>
-    <p>Redireccionando a <a href='$url'>$url</a></p>
+    <p id='backend-url'>$url</p>
   </body>
 </html>
 "@
 
-Set-Content -Path "index.html" -Value $redirect
+Set-Content -Path "index.html" -Value $html
 
 git add index.html
-git commit -m "Update redirect to $url"
+git commit -m "Update backend URL to $url"
 git push
